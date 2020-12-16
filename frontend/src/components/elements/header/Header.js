871f6a529d886b894790b102693eacae5a5adaec
Nathan Delavictoire <nathan.delavictoire@gmail.com>
2020-12-16 22:12:34 +0100

    Ajout du front pour le dashboard. En-tête et menu gauche  sont fait

--- frontend/src/components/elements/header/Header.js
+++ frontend/src/components/elements/header/Header.js
@@ -1,34 +1,46 @@
 import React from 'react';
-//import { Header} from '@gouvfr/header/src/scripts';
+import HeaderBrand from '../header-brand/HeaderBrand';
 
-const Dashboard = () => {
+const Header = ({serviceName,adminName,username,userSailorNumber}) => {
   return (
     <header className="rf-header">
-      <div className="rf-container">
+      <div className="rf-container rf-container__header">
         <div className="rf-header__body">
-          <div className="rf-header__brand">
-            <a className="rf-logo" href="#" title="République française">
-              <span className="rf-logo__title">
-                République
-                <br />
-                française
-              </span>
-            </a>
-          </div>
+
+        < HeaderBrand 
+        administrationLabel1={'Ministère'} administrationLabel2={'de la mer'}/>
+      
           <div className="rf-header__navbar">
             <div className="rf-service">
               <a className="rf-service__title" href="#" title="Nom du service">
-                Nom du service
+               {serviceName}
               </a>
               <p className="rf-service__tagline">
-                baseline - précisions sur l‘organisation
+                {adminName}
               </p>
             </div>
           </div>
+
+          <div class="rf-header__tools">
+            
+            <div class="cq-user">
+
+              <div class='cq-user__name'>{username}</div>
+              <div class="cq-user__sailor-number">
+                <span class="label">identifiant :</span><span class="nb">{userSailorNumber}</span>
+              </div>
+
+            </div>  
+
+            <div className="cq-user__picture">  
+            </div>
+
+          </div>
+
         </div>
       </div>
     </header>
   );
 };
 
-export default Dashboard;
+export default Header;
--- frontend/src/components/pages/dashboard/Dashboard.js
+++ frontend/src/components/pages/dashboard/Dashboard.js
@@ -7,11 +7,121 @@ import './Dashboard.scss';
 import sailorPicMock from '../../../resources/img/mocks/sailor.png';
 import menuPicMock from '../../../resources/img/mocks/menu.png';
 import TitleCard from '../../elements/cards/title-card/TitleCard';
+import Header from '../../elements/header/Header';
+
 
 const Dashboard = () => {
+
+  const user = store.getState().sailors.sailorBasicData.sailorCivilData;
+  const username = user.firstName + ' ' + user.lastName;
+  const userSailorNumber = user.sailorNumber;
+  console.log(user);
+
   return (
-    <div id="dashboard">
-      <nav>
+    <div id="accueil" className="page">
+      <Header serviceName={'CapQualif'} adminName={'Direction des affaires maritimes'} username={username} userSailorNumber={userSailorNumber}/>
+        
+        <div className="cq-section-header rf-container">
+  
+          <div class="cq-content-left">
+            <div class="cq-welcome">
+              
+              <h1>Bonjour {user.firstName}</h1> 
+        
+              <h4>Bienvenue sur <strong>CapQualif !</strong></h4>
+            </div>
+          </div>
+
+          <div class="cq-content-right">
+              <button class="rf-btn cq-upper" title="Demander un titre">
+                Demander un titre
+              </button>
+          </div>
+
+
+        </div>
+
+      <header class="rf-header no-shadow" >
+        <div class="rf-container">
+        
+        <nav class="rf-nav cq" role="navigation" aria-label="Menu principal">
+          <ul class="rf-nav__list">
+              <li class="rf-nav__item rf-nav__item--active">
+                <a class="rf-link" href="#" target="_self">Mes titres</a>
+              </li>
+              <li class="rf-nav__item">
+                <a class="rf-link" href="#" target="_self">Mon dossier</a>
+              </li>
+              <li class="rf-nav__item">
+                <a class="rf-link" href="#" target="_self">Mes informations personelles</a>
+              </li>
+          </ul>
+        </nav>
+
+
+        </div>
+      </header>
+
+      <div class="cq-content rf-container">
+        <div class="rf-grid-row">
+          <div class="rf-col-3 cq-left-3">
+            <ul class="cq-sidemenu">
+            <li class="rf-nav__item rf-nav__item--active cq-nav__vertical-item">
+            <a class="rf-link" href="#" target="_self">Mes Demande en cours</a>
+              </li>
+              <li class="rf-nav__item rf-nav__item--active cq-nav__vertical-item">
+              <a class="rf-link" href="#" target="_self">Fonction principales</a>
+              </li>
+              <li class="rf-nav__item cq-nav__vertical-item">
+              <a class="rf-link" href="#" target="_self">Fonction secondaire</a>
+              </li>
+              <li class="rf-nav__item cq-nav__vertical-item">
+              <a class="rf-link" href="#" target="_self">Diplômes</a>
+              </li>
+              <li class="rf-nav__item cq-nav__vertical-item">
+              <a class="rf-link" href="#" target="_self">Mes anciens titres</a>
+              </li>
+            </ul>
+          </div>
+
+
+          <div class="rf-col-9 cq-right-col-9">
+
+          <div id="applications-in-progress" class="cq-main-content">
+        <p className="title">Mes demandes en cours</p>
+        <ul className="card">
+          <li>CFBS : dossier en traitement ⏳</li>
+          <li>Premiers secours : dossier à compléter 🖊️</li>
+        </ul>
+      </div>
+      <div id="main-title">
+        <p className="title">Fonction principales</p>
+        <div className="card">Matelot Pont</div>
+      </div>
+      <div id="other-titles">
+        <p className="title">Mes autres titres</p>
+        <div id="other-titles-container">
+          {store
+            .getState()
+            .sailors.sailorBasicData.sailorEducationData.titles.map((title) => (
+              <TitleCard key={title.id} title={title} />
+            ))}
+        </div>
+      </div>
+
+
+
+          </div>
+        </div>
+
+
+      </div>
+
+      
+
+      {/*
+
+        <nav>
         <div id="photo">
           <img src={sailorPicMock} alt="a fake sailor's pic" />
         </div>
@@ -61,7 +171,12 @@ const Dashboard = () => {
           Ba blab blab la lorem ipsum fugu dzouigi dzouigi
         </div>
       </div>
-    </div>
+      
+      
+      
+      */}
+
+</div>    
   );
 };
 
