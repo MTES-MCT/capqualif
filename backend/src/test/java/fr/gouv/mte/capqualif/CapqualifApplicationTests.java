13259cec7cedaa8ae3a38a4f7a128e0d1d872fc1
elisabethfainstein <71322304+elisabethfainstein@users.noreply.github.com>
2021-02-16 14:35:13 +0100

    (test) add first contextLoads test for CapQualifApplication

--- backend/src/test/java/fr/gouv/mte/capqualif/CapqualifApplicationTests.java
+++ backend/src/test/java/fr/gouv/mte/capqualif/CapqualifApplicationTests.java
@@ -1,13 +1,21 @@
 package fr.gouv.mte.capqualif;
 
+import fr.gouv.mte.capqualif.instruction.adapters.in.web.CompareMarinDataToConditionsTitreController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CapqualifApplicationTests {
 
+	@Autowired
+	private CompareMarinDataToConditionsTitreController compareMarinDataToConditionsTitreController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(compareMarinDataToConditionsTitreController).isNotNull();
 	}
 
 }
