9e384341b1941fd2354c405e3d9efb496f2de6a8
elisabethfainstein <elisabeth.fainstein@developpement-durable.gouv.fr>
2021-05-21 17:19:57 +0200

    (fix) comment out DatabaseActions code because it makes sonar quality check fail but in reality  we won't use this code as it is

--- backend/src/main/java/fr/gouv/mte/capqualif/capadmin/adapters/out/persistence/temp/DatabaseActions.java
+++ backend/src/main/java/fr/gouv/mte/capqualif/capadmin/adapters/out/persistence/temp/DatabaseActions.java
@@ -31,28 +31,28 @@ public class DatabaseActions {
 //        System.out.println("------------------------ " + json);
 //        this.titreRepository.save(new TitreJpaEntity(titre.getName(), json));
 //    }
-
-    public void find(Long id) {
-        TitreJpaEntity titreJpa = this.titreRepository.findById(id).get();
-        System.out.println("!!!! titre found " + titreJpa);
-        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-        // https://stackoverflow.com/a/5554296
-        Type listType = new TypeToken<ArrayList<Condition>>(){}.getType();
-        List<Condition> list = new Gson().fromJson(titreJpa.getConditions(), listType);
-
-        Titre titre = new Titre(titreJpa.getTitre(), list);
-        System.out.println(titre);
-
-        Marin marin = new Marin(Arrays.asList(
-                new Data<String>("age", "21"),
-                new Data<String>("aptitude", "apte"),
-                new Data<List<String>>("formations", Arrays.asList("Module P1-Appui navigation"
-                        , "Module P2-Appui manutention et arrimage de la cargaison, pêche")),
-                new Data<List<String>>("titres", Arrays.asList("CFBS"))
-        )
-        );
-
-        ParseResult result = evaluationService.processTitre(titre, marin);
-        System.out.println(result);
-    }
+//
+//    public void find(Long id) {
+//        TitreJpaEntity titreJpa = this.titreRepository.findById(id).get();
+//        System.out.println("!!!! titre found " + titreJpa);
+//        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+//        // https://stackoverflow.com/a/5554296
+//        Type listType = new TypeToken<ArrayList<Condition>>(){}.getType();
+//        List<Condition> list = new Gson().fromJson(titreJpa.getConditions(), listType);
+//
+//        Titre titre = new Titre(titreJpa.getTitre(), list);
+//        System.out.println(titre);
+//
+//        Marin marin = new Marin(Arrays.asList(
+//                new Data<String>("age", "21"),
+//                new Data<String>("aptitude", "apte"),
+//                new Data<List<String>>("formations", Arrays.asList("Module P1-Appui navigation"
+//                        , "Module P2-Appui manutention et arrimage de la cargaison, pêche")),
+//                new Data<List<String>>("titres", Arrays.asList("CFBS"))
+//        )
+//        );
+//
+//        ParseResult result = evaluationService.processTitre(titre, marin);
+//        System.out.println(result);
+//    }
 }
