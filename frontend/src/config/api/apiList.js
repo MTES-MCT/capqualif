18b4dc76a4c2df891cc29087347ca77c630437ef
elisabethfainstein <elisabeth.fainstein@developpement-durable.gouv.fr>
2021-06-01 16:38:02 +0200

    (refactor) clean files

--- frontend/src/config/api/apiList.js
+++
@@ -1,4 +0,0 @@
-export const CAPQUALIF_URL = '***REMOVED***';
-export const SAILORS_ENDPOINT = 'sailors';
-export const TITRES_ENDPOINT = 'titres';
-export const ALL_TITRES_ENDPOINT = 'titres/titres';
