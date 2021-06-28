351c50256012502b557f6a379fc8eab4dc6d3495
elisabethfainstein <elisabeth.fainstein@developpement-durable.gouv.fr>
2021-06-28 14:57:24 +0200

    (refactor) add explaning comment

--- backend/src/main/java/fr/gouv/mte/capqualif/capqualif/instruction/domain/MarinDataNames.java
+++ backend/src/main/java/fr/gouv/mte/capqualif/capqualif/instruction/domain/MarinDataNames.java
@@ -1,11 +1,13 @@
 package fr.gouv.mte.capqualif.capqualif.instruction.domain;
 
+/**
+ * This values are used in the titre's conditions jsons. They are leftOpId values.
+ */
 public enum MarinDataNames {
     ADMINISTRES("age"),
     ESCULAPE("aptitude"),
     AMFORE("formations"),
     ITEM("titres");
-
 
     private final String name;
 
